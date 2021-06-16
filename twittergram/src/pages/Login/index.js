6d96b046c58063670eb968b5fedac3e2b449f33b
thiagobonfim0310 <thiagofla.f@gmail.com>
2021-06-15 22:45:04 -0300

    antd add

--- twittergram/src/pages/Login/index.js
+++ twittergram/src/pages/Login/index.js
@@ -1,7 +1,32 @@
-import React, {Text} from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+
 import './index.css'
 import Home from '../../assets/images/home.png'
+
 export default props => {
+    const layout = {
+        labelCol: {
+          span: 8,
+        },
+        wrapperCol: {
+          span: 16,
+        },
+      };
+      const tailLayout = {
+        wrapperCol: {
+          offset: 8,
+          span: 16,
+        },
+      };
+      const Demo = () => {
+        const onFinish = (values) => {
+          console.log('Success:', values);
+        };
+      
+        const onFinishFailed = (errorInfo) => {
+          console.log('Failed:', errorInfo);
+        };
+    }
 
     return (
         <div className= "container">
@@ -11,9 +36,54 @@ export default props => {
             <div className= "body">
                 <img src= {Home} className= "image"/>
                 <div className = "form">
-                    
+                <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
                 </div>
             </div>
         </div>
     )
 } 
+
