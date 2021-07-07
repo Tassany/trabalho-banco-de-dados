70784273ff9128d85f32b7514e377f9975e3ee2f
tassany <tassanyonofre@gmail.com>
2021-07-07 12:58:13 -0300

    Novos componentes

---
+++ twittergram/src/components/Post/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+
+const Post = () => (
+    <div className="post-class">
+        {/* <div className="rectangle-post"></div> */}
+        <img className="photo-post" src="https://static.vecteezy.com/ti/fotos-gratis/p1/703357-bom-pequeno-amarelo-pato-retrato-isolado-no-branco-foto.jpg"></img>
+
+        <span>Tags:</span>
+    </div>
+)
+
+export default Post
--- twittergram/src/pages/Feed/index.jsx
+++ twittergram/src/pages/Feed/index.jsx
@@ -2,14 +2,16 @@
 import React from  'react'
 import './index.css'
 
-import Container from '../../components/container'
-import Header from '../../components/header'
+import Container from '../../components/Container'
+import Header from '../../components/Header'
+import Post from '../../components/Post'
 export default function Feed(){
   return(
     <>
       <Header />
       <Container>
           <h1>Feed</h1>
+          <Post></Post>
       </Container>
 
     </>
