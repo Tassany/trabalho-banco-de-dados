f3f4c4de99536412827d771ebc42a39a1413ff5e
thiago <you@example.com>
2021-07-08 11:50:52 -0300

    Tela Login e Cadastro

--- twittergram/src/pages/Feed/index.jsx
+++ twittergram/src/pages/Feed/index.jsx
@@ -1,27 +1,23 @@
 /* eslint-disable no-undef */
-import React from  'react'
-import {useState, useEffect} from 'react';
-import './index.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./index.css";
 
-import Container from '../../components/Container'
-import Header from '../../components/Header'
-import teste from '../../data/teste'
-import Post from '../../components/Post'
-import axios from 'axios';
+import Container from "../../components/Container";
+import Header from "../../components/Header";
+import teste from "../../data/teste";
+import Post from "../../components/Post";
+import axios from "axios";
 
-
-
-export default function Feed(){
-
+export default function Feed() {
   // const [posts, setPosts] = useState([]);
 
   // useEffect(() => {
   //   refreshPosts();
   //     return () => {
-          
+
   //     }
   // }, [])
-
 
   // async function refreshPosts(){
   //     Server.getAllUsers()
@@ -30,37 +26,38 @@ export default function Feed(){
   //                 setPosts(response.data)
   //             }
   //         )
-      
+
   // }
   state = {
-    users: []
-  }
+    users: [],
+  };
 
-  componentDidMount(){
-    axios.get(`http://localhost:5000/users`)
-      .then(res => {
-        const users = res.data;
-        this.setState({ users });
-      })
-  }
+  // componentDidMount(){
+  //   axios.get(`http://localhost:5000/users`)
+  //     .then(res => {
+  //       const users = res.data;
+  //       this.setState({ users });
+  //     })
+  // }
 
   // const lis = teste.map(teste => {
   //   return <Post titulo={teste.name}> </Post>
   // })
-  return(
+  return (
     <>
       <Header />
       <Container>
-          <h1>Feed</h1>
+        <h1>Feed</h1>
 
-                <ul>
-        { this.state.users.map(user => <li>{user.name}</li>)}
-      </ul>
-          {/* <h2>{posts}</h2>
+        <ul>
+          {this.state.users.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </ul>
+        {/* <h2>{posts}</h2>
           {lis} */}
-          {/* <Post></Post> */}
+        {/* <Post></Post> */}
       </Container>
-
     </>
   );
 }
--- twittergram/src/pages/Login/index.js
+++ twittergram/src/pages/Login/index.js
@@ -1,58 +1,176 @@
-import React from  'react'
+import React, { useState } from "react";
+import axios from "axios";
+import "./index.css";
+import Home from "../../assets/images/home.png";
+const API_URL = "http://localhost:5000";
+const initialFormState = {
+  name: "",
+  email: "",
+  senha: "",
+};
 
-import './index.css'
-import Home from '../../assets/images/home.png'
+export default function Login(props) {
+  const [form, setForm] = useState(initialFormState);
+  const [stageNew, setStageNew] = useState(false);
+  const [label, setlabel] = useState(false);
+  const [label2, setlabel2] = useState(false);
+  const clear = () => {
+    setForm(initialFormState);
+  };
 
-export default function Login(){
-    const layout = {
-        labelCol: {
-          span: 8,
-        },
-        wrapperCol: {
-          span: 16,
-        },
-      };
-      const tailLayout = {
-        wrapperCol: {
-          offset: 8,
-          span: 16,
-        },
-      };
-      const Demo = () => {
-        const onFinish = (values) => {
-          console.log('Success:', values);
-        };
-      
-        const onFinishFailed = (errorInfo) => {
-          console.log('Failed:', errorInfo);
-        };
-    }
+  const auth = () => {
+    axios(API_URL, { email: form.email, senha: form.senha }).then((resp) => {
+      resp = JSON.parse(resp);
+      if (resp) {
+        return props.history.push({
+          pathname: "/feed",
+          state: { data: resp.data },
+        });
+      } else {
+        clear();
+        setlabel(true);
+      }
+    });
+  };
+  const register = () => {
+    axios
+      .post(API_URL, { name: form.name, email: form.email, senha: form.senha })
+      .then((resp) => {
+        resp = JSON.parse(resp);
 
-    return (
-        <div className= "container">
-            <div className = "cont-text">
-                <h1 className = "title" >TWITERGRAM</h1>
+        if (resp) {
+          setlabel2(false);
+          setlabel(false);
+          setStageNew(false);
+        } else {
+          clear();
+          setlabel2(true);
+        }
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="cont-text">
+        <h1 className="title">TWITERGRAM</h1>
+      </div>
+      <div className="body">
+        <img src={Home} className="image" />
+        <form className="form">
+          <div
+            style={{
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              textAlign: "center",
+              width: "100%",
+              paddingBottom: "12px",
+            }}
+          >
+            {label2 && (
+              <label
+                style={{
+                  textAlign: "center",
+                  color: "red",
+                  fontSize: "15px",
+                }}
+              >
+                Usuário já castrado
+              </label>
+            )}
+          </div>
+          {stageNew && (
+            <div className="form-group email">
+              <label>Nome</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={form.name}
+                onChange={(e) => setForm(e.target.value)}
+                placeholder="Digite o name"
+              />
             </div>
-            <div className= "body">
-                <img src= {Home} className= "image"/>
-                
-                <form className = "form">
-                    
-                        <label className="email" placeholder= 'email' >
-                            
-                            <input type= "text" name="email"/>
-                        </label>
-                        <label className="senha" placeholder= 'senha' >
-                            
-                            <input type= "text" name="senha"/>
-                        </label>
+          )}
+          <div
+            style={{
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              textAlign: "center",
+              width: "100%",
+              paddingBottom: "12px",
+            }}
+          >
+            {label && (
+              <label
+                style={{
+                  textAlign: "center",
+                  color: "red",
+                  fontSize: "15px",
+                }}
+              >
+                Verifique se email e senha estão corretos
+              </label>
+            )}
+          </div>
+          <div className="form-group email">
+            <label>Email</label>
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              value={form.email}
+              onChange={(e) => setForm(e.target.value)}
+              placeholder="Digite o email"
+            />
+          </div>
 
-                        <button className="singin" type="submit">Entrar</button>
+          <div className="form-group senha">
+            <label>Senha</label>
+            <input
+              type="password"
+              className="form-control"
+              name="senha"
+              value={form.senha}
+              onChange={(e) => setForm(e.target.value)}
+              placeholder="Digite o senha"
+            />
+          </div>
 
-                        <button className="register" type="submit">Cadastrar</button>
-                </form>
-               
-            </div>
-        </div>
-    )
+          {!stageNew && (
+            <button
+              className="singin btn btn-primary"
+              type="button"
+              onClick={() => auth()}
+            >
+              Entrar
+            </button>
+          )}
+
+          {!stageNew && (
+            <button
+              className="register"
+              type="button"
+              onClick={() => {
+                setStageNew(true);
+                setlabel2(false);
+                setlabel(false);
+              }}
+            >
+              Cadastrar
+            </button>
+          )}
+
+          {stageNew && (
+            <button
+              className="singin btn btn-primary"
+              type="button"
+              onClick={() => register()}
+            >
+              Registrar
+            </button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
